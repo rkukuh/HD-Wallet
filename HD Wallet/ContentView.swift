c9a88fd79187118f61a91c88211db5bd803f970e
R. Kukuh <rkukuh@gmail.com>
2023-09-27 21:39:50 +0700

    Initial Commit

---
+++ HD Wallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HD Wallet
+//
+//  Created by R. Kukuh on 27/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
